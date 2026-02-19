2e16255420964fd1264b02945d893dd3a03168ec
Chenna <2783031@tcsgenailab.onmicrosoft.com>
2026-02-19 11:02:32 +0530

    main.py updated for user

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from database import engine
 import models
-from routers import auth, todos, admin
+from routers import auth, todos, admin, users
 
 
 app = FastAPI ()
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
+app.include_router(users.router)
 # this includes all routers in auth file
 
 
